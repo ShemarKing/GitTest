7d993aca25760485e7cfb659ccbe0d81c26e0b18
Zhangyx <zhangyx@ZhangyxdeMacBook-Pro.local>
2019-03-11 13:55:59 +0800

    Initial Commit

---
+++ RuntimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeTest
+//
+//  Created by Zhangyx on 2019/3/11.
+//  Copyright © 2019 Zhangyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RuntimeTest
+//
+//  Created by Zhangyx on 2019/3/11.
+//  Copyright © 2019 Zhangyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
